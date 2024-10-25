68094b0c455a73c9157ca99749d6ad9817032bff
jlekshmi <jayalekshmivj08@gmail.com>
2024-10-25 15:01:15 -0400

    Login page

--- HomeBite/client/src/Pages/Login/Login.js
+++ HomeBite/client/src/Pages/Login/Login.js
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 import loginbg from "../../assets/images/login-bg.jpg";
 import Logo from "../../assets/images/logo.svg";
+import InputField from "../../Components/InputField/InputField";
+import Button from "../../Components/Button/Button";
+import { Container, Row, Col, Alert } from "react-bootstrap";
 
 const Login = () => {
     const [loginData, setLoginData] = useState({ username: '', password: '' });
@@ -38,48 +41,39 @@ const Login = () => {
     };
 
     return (
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-md-7 p-0'>
+        <Container fluid>
+            <Row>
+                <Col md={7} className="p-0">
                     <div className="login-container">
                         <div className="login-box">
                             <img src={Logo} className="logo" alt="Logo" />
                             <h2 className="form-title mt-5 mb-2">Sign In to HomeBite</h2>
-                            <h4 className="form-sub-title mb-3">Sign in to enjoy the best home-cooked meals made with care!</h4>
+                            <h4 className="form-sub-title mb-3">
+                                Sign in to enjoy the best home-cooked meals made with care!
+                            </h4>
+                            {message && <Alert variant="danger">{message}</Alert>}
                             <form onSubmit={handleSubmit}>
-                                <div className="input-group mb-0">
-                                    <label htmlFor="username" className="input-label">Your Email Address</label>
-                                    <input
-                                        type="text"
-                                        id="username"
-                                        name="username"
-                                        className="input-field"
-                                        value={loginData.username}
-                                        onChange={handleChange}
-                                        placeholder="Your Email Address"
-                                        required
-                                    />
-                                    <p className="form-text">Please enter the email associated with your account.</p>
-                                </div>
-
-                                <div className="input-group">
-                                    <label htmlFor="password" className="input-label">Your Password</label>
-                                    <input
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        className="input-field"
-                                        value={loginData.password}
-                                        onChange={handleChange}
-                                        placeholder="Your Password"
-                                        required
-                                    />
-                                </div>
-
-                                <button type="submit" className="btn btn-primary w-100 mb-3">Let's Go!</button>
-
-                                {message && <p className="form-message">{message}</p>}
-
+                                <InputField
+                                    label="Your Email Address"
+                                    name="username"
+                                    type="text"
+                                    value={loginData.username}
+                                    onChange={handleChange}
+                                    placeholder="Your Email Address"
+                                    required
+                                />
+                                <InputField
+                                    label="Your Password"
+                                    name="password"
+                                    type="password"
+                                    value={loginData.password}
+                                    onChange={handleChange}
+                                    placeholder="Your Password"
+                                    required
+                                />
+                                <Button type="submit" className="btn-primary w-100 mb-3">
+                                    Let's Go!
+                                </Button>
                                 <div className="auth-links d-flex justify-content-between">
                                     <p>
                                         New to HomeBite? <a href="/register" className="btn btn-link">Create an account</a>
@@ -89,27 +83,26 @@ const Login = () => {
                             </form>
                         </div>
                     </div>
-                    <footer>
-                        <div className='d-flex justify-content-between footer-container'>
-                            <p>HomeBite © All Rights Reserved</p>
-                            <div className='d-flex link-color'><i className='material-icons mx-2'>email</i><p className='align-middle'>help@homebite.com</p></div>
-                        </div>
-                    </footer>
-                </div>
-
-                <div className='col-md-5 d-flex align-items-center justify-content-center p-0 position-relative'>
+                </Col>
+                <Col md={5} className="d-flex align-items-center justify-content-center p-0 position-relative">
                     <div className="overlay position-absolute w-100 h-100"></div>
                     <img src={loginbg} className="img-fluid login-bg w-100 h-100" alt="Background" />
                     <div id="textCarousel" className="carousel slide position-absolute mb-3" data-bs-ride="carousel">
                         <div className="carousel-inner w-50">
                             <div className="carousel-item active">
-                                <div className="d-block p-3 text-white h3 text-carousel">“Home-cooked meals, made by the community, for the community.”</div>
+                                <div className="d-block p-3 text-white h3 text-carousel">
+                                    “Home-cooked meals, made by the community, for the community.”
+                                </div>
                             </div>
                             <div className="carousel-item">
-                                <div className="d-block p-3 text-white h3 text-carousel">Second Text Slide</div>
+                                <div className="d-block p-3 text-white h3 text-carousel">
+                                    Second Text Slide
+                                </div>
                             </div>
                             <div className="carousel-item">
-                                <div className="d-block p-3 text-white h3 text-carousel">Third Text Slide</div>
+                                <div className="d-block p-3 text-white h3 text-carousel">
+                                    Third Text Slide
+                                </div>
                             </div>
                         </div>
                         <div className='position-relative w-50 mx-3 d-flex'>
@@ -123,9 +116,18 @@ const Login = () => {
                             </button>
                         </div>
                     </div>
+                </Col>
+            </Row>
+            <footer>
+                <div className='d-flex justify-content-between footer-container'>
+                    <p>HomeBite © All Rights Reserved</p>
+                    <div className='d-flex link-color'>
+                        <i className='material-icons mx-2'>email</i>
+                        <p className='align-middle'>help@homebite.com</p>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </footer>
+        </Container>
     );
 };
 
